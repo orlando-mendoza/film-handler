98fbefc2a0cc7003209997d783c3d6ffe6f94494
Orlando Mendoza <orlandomr27@gmail.com>
2020-09-26 08:05:11 -0600

    Added index page

---
+++ src/film_ratings/views/index.clj
@@ -0,0 +1,13 @@
+(ns film-ratings.views.index
+  (:require
+   [film-ratings.views.template :refer [page]]))
+
+(defn list-options []
+  (page
+   [:div.container.jumbotron.bg-white.text-center
+    [:row
+     [:p
+      [:a.btn.btn-primary {:href "/add-film"} "Add a Film"]]]
+    [:row
+     [:p
+      [:a.btn.bt-primary {:href "/list-films"} "List Films"]]]]))
